8551d377f34c9db59de2409e3e1275405315fd87
Badamgarav <badamaa611@gmail.com>
2026-05-13 21:06:58 +0800

    hh

--- app.py
+++ app.py
@@ -37,3 +37,8 @@ def create_app():
         angi = request.args.get("angi", "12")
         hicheel = request.args.get
         app = create_app()
+        app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
