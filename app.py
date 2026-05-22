c1505543a2107b7de0c489c36aec9b72c44e7b79
Badamgarav <badamaa611@gmail.com>
2026-05-22 21:20:13 +0800

    бчёөс

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-﻿from flask import Flask, render_template, request, redirect, url_for, flash, session
+﻿from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import requests, csv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-brain-secret-key-2026'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superbrain.db'
+
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
@@ -31,20 +31,25 @@ class TestResult(db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 @login_required
 def index():
+    # Таны анхны загварт байсан ангиудын өгөгдөл
+    grade_subjects = {'5': ['Математик', 'Монгол хэл'], '9': ['Физик', 'Англи хэл'], '12': ['Математик', 'Физик']}
     chats = ChatHistory.query.filter_by(user_id=current_user.id).all()
     results = TestResult.query.filter_by(user_id=current_user.id).all()
-    # Энд өөрийн ангиудын логикоо нэмнэ
-    grade_subjects = {'5': ['Математик', 'Монгол хэл'], '9': ['Физик', 'Англи хэл']}
+    
+    # Эндээс та өөрийн анхны index.html-ээ дуудна
     return render_template('index.html', chats=chats, results=results, grade_subjects=grade_subjects)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         user = User.query.filter_by(username=request.form['username']).first()
-        if user:
+        if user and user.password == request.form['password']:
             login_user(user)
             return redirect(url_for('index'))
     return render_template('login.html')
@@ -55,5 +60,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    with app.app_context(): db.create_all()
-    app.run(debug=True)
+    app.run()
